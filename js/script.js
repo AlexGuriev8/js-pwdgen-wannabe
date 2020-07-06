b9e706f61a0fbfcf3c0a67867bcf22066d616656
Alex <gurievalex78@gmail.com>
2020-07-06 18:20:55 +0200

    Init

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var name = prompt('Inserisci il tuo nome:');
+var surname = prompt('Inserisci il tuo cognome:');
+var color= prompt('Inserisci il tuo Colore preferito:');
+
+var somma = name + surname + color + 19;
+document.getElementById('somm1').innerHTML = 'La tua password è : ' + somma;
+
+// console.log(name);
+// console.log(surname);
+// console.log(color);
